d55b833d87ce96694ed286525045d466b0724aca
wilmardo <info@wilmardenouden.nl>
2018-09-26 14:30:51 +0200

    cleanup(Dockerfile): Removes unused profile

--- profile.ps1
+++
@@ -1,4 +0,0 @@
-New-Item /dev/shm/.local/share/powershell/PSReadLine/ -ItemType Directory
-New-Item /dev/shm/.local/share/powershell/PSReadLine/ConsoleHost_history.txt -type file
-Set-PSReadlineOption -HistorySavePath /dev/shm/ConsoleHost_history.txt
-Set-PSReadlineKeyHandler -Key Tab -Function Complete
